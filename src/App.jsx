76a92538d343261b2e3341fa1a2daf93c8988802
Marwa MAAMOURI <MAAMOURI@enda.tmw>
2020-06-24 10:32:30 +0100

    loginTest

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+
+import LoginPage from './components/LoginPage'
+ 
+
+function App() {
+  return (
+    
+
+    <LoginPage>
+    </LoginPage>
+
+
+<p>AppJsFile</p>
+    
+  );
+}
+
+export default App;
